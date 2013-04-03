941b7aca6f4c4598b1eabde7d5d1d3914b473194
David Cournapeau <cournape@gmail.com>
2013-04-03 17:34:54 +0100

    REL: version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 if __name__ == "__main__":
     setup(name="machotools",
-          version="0.0.1",
+          version="0.0.2",
           author="David Cournapeau",
           author_email="cournape@gmail.com",
           license="BSD",
