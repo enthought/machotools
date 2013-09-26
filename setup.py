ed3cf9890f4066fe37bf0669eacf96979c8b5cbf
David Cournapeau <cournape@gmail.com>
2013-09-26 08:33:46 +0100

    MAINT: 0.1.0 release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os.path as op
 
 from setuptools import setup
 
-VERSION = "0.1.0.dev1"
+VERSION = "0.1.0"
 
 if __name__ == "__main__":
     with open(op.join("machotools", "__version.py"), "wt") as fp:
