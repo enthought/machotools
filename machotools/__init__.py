cf18c98501811d4c3af31e82f3a04e6a196c83eb
David Cournapeau <cournape@gmail.com>
2014-03-06 14:22:54 +0000

    STY: remove doubly defined __version__.

--- machotools/__init__.py
+++ machotools/__init__.py
@@ -1,11 +1,8 @@
 # Copyright (c) 2013 by Enthought, Ltd.
 # All rights reserved.
-__version__ = "0.1.0.dev1"
-
 from machotools.macho_rewriter import BundleRewriter, DylibRewriter, \
     ExecutableRewriter, rewriter_factory
 
-# Silent pyflakes vim plugin
 __all__ = [
     "BundleRewriter", "DylibRewriter", "ExecutableRewriter",
     "rewriter_factory",
