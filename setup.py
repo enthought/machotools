69a7e82e5a5f46f446be13b96c25e558543486c0
David Cournapeau <cournape@gmail.com>
2017-02-06 15:47:43 +0000

    MAINT: update description metadata.

--- setup.py
+++ setup.py
@@ -10,10 +10,15 @@ if __name__ == "__main__":
     with open(os.path.join("machotools", "__version.py"), "wt") as fp:
         fp.write("__version__ = '{0}'".format(VERSION))
 
+    with open("README.rst", "rt") as fp:
+        long_description = fp.read()
+
     setup(name="machotools",
           version=VERSION,
           author="David Cournapeau",
           author_email="cournape@gmail.com",
+          description="A small library + CLI tool to manipulate Mach-0 binaries",
+          long_description=long_description,
           license="BSD",
           packages=["machotools", "machotools.tests"],
           install_requires=["macholib"],
